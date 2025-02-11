e0233a869f49a8f8d5bc4e3f63d38bfee3d86cca
Daniel José dos Santos <djdossantos@live.com>
2025-02-11 13:21:50 -0300

    list expression

--- packages/boxed-expression-component/src/expressions/ListExpression/ListExpression.tsx
+++ packages/boxed-expression-component/src/expressions/ListExpression/ListExpression.tsx
@@ -116,7 +116,7 @@ export function ListExpression({
   const beeTableRows = useMemo(() => {
     const rows = (listExpression.expression ?? []).map((item) => ({
       "@_id": generateUuid(),
-      expression: item,
+      expression: item?.__$$element === undefined ? undefined! : item,
     }));
 
     if (rows.length === 0) {
